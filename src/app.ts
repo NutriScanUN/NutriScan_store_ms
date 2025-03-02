8e1db1b2cb18026a2e2cfef9e9ff19ece943153e
Juan Carreño <jcarrenoar@unal.edu.co>
2025-03-02 14:07:54 -0500

    add prometheus route /metrics

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import routes from "./routes.js";
 import sequelize from "./database.js";
 import { SyncOptions } from "sequelize";
 import cors from "cors";
+import promClient  from "prom-client";
+
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
 
 const syncOptions:SyncOptions = {
   force: (process.env.SQLIZE_FORCE === "true"),
@@ -22,6 +27,11 @@ app.use(cors());
 
 app.use(express.json())
 app.use("/api", routes);
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 sequelize.sync(syncOptions).then( val => {
   
